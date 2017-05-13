53136c08f7ae23d53ebd6d01fa165910c44b7ae6
Vas <vhatzopoulos@gmail.com>
2017-05-13 12:46:56 +0100

    minor edit for typos and comments

--- get_comments.py
+++ get_comments.py
@@ -8,19 +8,20 @@ from elasticsearch import Elasticsearch
 
 # Setup praw
 load_dotenv(find_dotenv())
-CLIENT_SECRET = os.environ.get("SECRET")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 CLIENT_ID = os.environ.get("CLIENT_ID")
-USER_AGENT = os.environ.get("USER_aGENT")
+USER_AGENT = os.environ.get("USER_AGENT")
 
 reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent=USER_AGENT)
 print(reddit.read_only)
 
 
 # Setup ES
+index="rjobs"
 es = Elasticsearch()
-if es.indices.exists("rjobs"):
-    es.indices.delete(index="rjobs")
-res = es.indices.create(index="rjobs")
+if es.indices.exists(index):
+    es.indices.delete(index=index)
+res = es.indices.create(index=index)
 
 es.indices.put_mapping(
 	index="rjobs",
@@ -55,8 +56,12 @@ es.indices.put_mapping(
 
 num_comments = 0
 subreddit = 'jobs'
-for submission in reddit.subreddit(subreddit).top(limit=None):
 
+
+# loop through all submissions and all comments within them
+for submission in reddit.subreddit(subreddit).top(limit=None):
+	
+	# take care of nested comments 
 	submission.comments.replace_more(limit=0)
 	for comment in submission.comments.list():
 		dt = datetime.datetime.fromtimestamp(comment.created).strftime('%Y-%m-%d')
